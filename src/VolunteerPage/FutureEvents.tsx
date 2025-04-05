1361427a8b75e266a7373eb6dea196f01ef91661
maxbek97 <maxbek294@gmail.com>
2025-04-05 13:57:59 +0300

    Added buttons and global styles

---
+++ src/VolunteerPage/FutureEvents.tsx
@@ -0,0 +1,12 @@
+// Примеры компонентов
+import React, { useState } from 'react';
+
+
+const FutureEvents: React.FC = () => (
+    <ul className="list-disc pl-5">
+      <li>Frontend Meetup 2025</li>
+      <li>Node.js Workshop</li>
+    </ul>
+  );
+
+export default FutureEvents;
---
+++ src/VolunteerPage/MyEvents.tsx
@@ -0,0 +1,14 @@
+// Примеры компонентов
+import React, { useState } from 'react';
+
+
+const MyEvents: React.FC = () => {
+    return (
+  <ul className="list-disc pl-5">
+    <li>Подписка на конференцию</li>
+    <li>Мастер-класс по React</li>
+  </ul>
+  )
+};
+
+export default MyEvents;
