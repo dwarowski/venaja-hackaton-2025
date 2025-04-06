a3c9a6f85d999d8aac6b5cbaa393d83e408e2b20
Tretchian <safonovila7@gmail.com>
2025-04-06 08:58:47 +0300

    logout

--- src/pages/OrganiserPage/organiser.tsx
+++ src/pages/OrganiserPage/organiser.tsx
@@ -14,18 +14,6 @@ import { useAuthContext } from "../../AuthComponent/auth";
 
 const OrganiserPage: React.FC = () => {
   const { keycloak , userInfo } = useAuthContext();
-  
-  <header className="App-header">
-        <img src="/upper_line.svg" className="header-image" alt='abiba'/>
-        <div className='functional-header'>
-          <a href="http://www.sberbank.ru"><img src="/logo.svg" className = "header-logo" alt='abiba'/></a>
-          <Button onClick={() => keycloak.logout()}>
-          Logout
-          </Button>
-        </div>
-      </header>
-
-
   const [activeTab, setActiveTab] = useState<'upcoming' | 'pending' | 'completion'>('upcoming');
   const [showNewEventForm, setShowNewEventForm] = useState(false);
   const [events, setEvents] = useState<Event[]>([
@@ -229,6 +217,11 @@ const [eventRequests, setEventRequests] = useState<EventRequest[]>([
 
   return (
     <div className="organiser-page">
+
+          <Button onClick={() => keycloak.logout()}>
+          Logout
+          </Button>
+
       <div className="tabs-container">
         <button
           className={`tab-button ${activeTab === 'upcoming' ? 'active' : ''}`}
